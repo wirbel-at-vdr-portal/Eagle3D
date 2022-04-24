4730682f535e1401298e415cd588b3fd9ba6f501
wirbel <womp@herr-der-mails.de>
2022-04-24 15:08:52 +0200

    screwed up sync script - fix it

--- synchronize.sh
+++ synchronize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rsync -a --delete "/C/Programme_2/POV-Ray-8;71M8;71m7;71M7;71m7;71M7;71m3.7/include/Eagle3D/" povray
+rsync -a --delete "/C/Programme_2/POV-Ray-v3.7/include/Eagle3D/" povray
 rsync -a --delete "/C/J_Programme/Eagle-7.6.0/ulp/" ulp
 
 git status
